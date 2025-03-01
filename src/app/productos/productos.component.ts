2d2999143ce7bf2ad093bd10ee4ad2dea6f52520
Abel <abel.alvarez@galileo.edu>
2025-02-28 19:22:05 -0600

    fix: :bug: Nuevas mejoras

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ModalDialogComponent } from '../components/modal-dialog/modal-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
@@ -9,9 +12,16 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, NgxDatatableModule,FormsModule],
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.scss']
+  styleUrls: ['./productos.component.scss', '../../styles.scss']
 })
 export class ProductosComponent {
+  // Modal
+  title = 'Productos';
+  message = 'Este es un mensaje dinámico.';
+  cancelButton = true;
+  actionButton = true;
+  actionButtonText = 'Añadir producto';
+  // Data
   rows = [
     { id:1 ,nombre: 'Harina de trigo', descripcion: 'Harina blanca refinada, 50 kg', precio: 25, marca: 'Molinos San Juan', categoria: 'Granos', unidad: 'Sacos' },
     { id:2 ,nombre: 'Aceite vegetal', descripcion: 'Aceite comestible, 5 litros', precio: 15, marca: 'Oro Verde', categoria: 'Aceites', unidad: 'Garrafas' },
@@ -21,6 +31,10 @@ export class ProductosComponent {
   ];
   filteredRows = [...this.rows]; // Copia inicial de los datos
   searchTerm = '';
+  cancelButtonText = 'Cancelar';
+  form!: FormGroup;
+
+  constructor(private dialog: MatDialog, private fb: FormBuilder) {}
 
   filterProducts() {
     const term = this.searchTerm.toLowerCase();
@@ -39,6 +53,46 @@ export class ProductosComponent {
   eliminarProducto(row: any) {
     console.log('Eliminar producto:', row);
     // Lógica para eliminar producto
+  }
+
+  abrirModal(): void {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      precio: ['', [Validators.required]],
+      marca: [0, [Validators.required]],
+      categoria: [0, [Validators.required]],
+      unimed: [0, [Validators.required]],
+    });
+
+    const dialogRef = this.dialog.open(ModalDialogComponent, {
+      width: '726px',
+      disableClose: false,
+      data: {
+        title: this.title,
+        columns: 1,
+        message: this.message,
+        showCancelButton: this.cancelButton,
+        cancelButtonText: this.cancelButtonText,
+        showActionButton: this.actionButton,
+        actionButtonText: this.actionButtonText,
+        form: this.form,
+        fields: [
+          { label: 'Nombre', name: 'nombre', type: 'text', placeholder: 'Nombre del producto' },
+          { label: 'Precio', name: 'precio', type: 'number', placeholder: 'Precio del producto' },
+          { label: 'Marca', name: 'marca', type: 'text', placeholder: 'Marca del producto' },
+          { label: 'Categoría', name: 'categoria', type: 'select',  },
+          { label: 'Unidad de medida', name: 'unimed', type: 'select' },
+        ],
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Acción principal confirmada');
+      } else {
+        console.log('Modal cerrado sin acción');
+      }
+    });
   }
 }
 
